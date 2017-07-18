87d8778c9e699c74b182bbb6ab3aa9bd09d78625
hls93 <hlshaw3@ncsu.edu>
2017-07-18 16:16:03 -0400

    making changes to js

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
-let gallery = document.querySelector('.gallery');
-let galleryHtml = '';
 
-console.log('hello world');
+let recipeBox = document.querySelector(".recipeContainer")
+let recipes = document.querySelector(".list")
+let content = document.querySelector("#container");
+let button = document.querySelector("#button");
+let search = document.querySelector("#input");
+
+button.addEventListener("click", function(){
+fetch(`https://crossorigin.me/http://www.recipepuppy.com/api/?q=${search.value}`)
+
+  .then(function(response) {
+    if (response.status === 200) {
+      return response.json();
+    }
+  })
+
+  .then(function(data) {
+    let results = data.results;
+    for (var i = 0; i < results.length; i++) {
+      recipes.innerHTML += `
+    <img src=${results[i].thumbnail}>
+    <a href=${results[i].href}><h1 class="title">${results[i].title}</h1></a>
+    `
+    }
+
+  })
+})
